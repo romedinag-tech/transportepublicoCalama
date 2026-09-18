c30e973c5be15afd58a9a75d4ec9ec35bd88319a
Rodrigo Medina <romedinag@gmail.com>
2026-09-18 09:26:44 -0300

    Deploy tablero calama desde kit compartido (v=205)

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ window.CITY = {
   comunasGeojson: "comunas_calama.geojson",
   live: false,
   liveBase: "",
-  demanda: false,
+  demanda: true,
   repo: "transportepublicoCalama",
   voz: {"ejeSing": "eje", "ejePlur": "ejes", "EjePlur": "Ejes"},
 };
